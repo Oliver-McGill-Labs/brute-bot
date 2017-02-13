dfa9157511ab3d7533b319d2e0cc86e7ec7c2ef7
Matt Oliver <mattoliver.mattoliver@gmail.com>
2017-02-13 11:08:58 -0600

    removing keys

--- env/api_keys.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = {
- consumer_key: '2ELLindNc3Njhv2ekNthARDPq',
- consumer_secret: 'p8XjvJUexcDimkBwJCPT6LtJGB9IhYHiKsUyyfJD5BomGlRv2A',
- access_token_key: '827932202243596288-ytEuKKyPbEbsR39LZrcgXo2531KBqGq',
- access_token_secret: 'Ivo8D9xC2SuC0pEGRlC8RcsP2suvlRafmJmyMSvtdD69P'
-};
